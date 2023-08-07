ccdff57649594afe48f2fd13b85cac08cafcd392
jhoitenga <116404784+jhoitenga@users.noreply.github.com>
2023-08-07 13:52:52 -0500

    Assignment 1.3

--- week-1/hello-world/src/app/app.component.ts
+++ week-1/hello-world/src/app/app.component.ts
@@ -1,3 +1,11 @@
+/**
+ * Title: app.component.html
+ * Author: Jennifer Hoitenga
+ * Date: August 7, 2023
+ * Sources Used:
+ * Angular Tutorial for Beginners video by Professor Krasso
+ */
+
 import { Component } from '@angular/core';
 
 @Component({
